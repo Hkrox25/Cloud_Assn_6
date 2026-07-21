a23f04408bb2f7849fde0c8e9d8951cb5ed128e2
Hkrox25 <hrishikeshk1325@gmail.com>
2026-07-21 15:01:05 +0530

    Correct 'system' to 'System' in Calc.java
    Fixed multiple instances of 'system' to 'System' and added missing newline at the end of the file.

---
+++ Calc.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Calc{
+  public static void main(String [] args){
+    int switchX, one, two,result=0;
+    boolean choice=true;
+    
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("------MENU------");
+    System.out.println("    1.  +");
+    System.out.println("    2.  -");
+    System.out.println("    3.  *");
+    System.out.println("    4.  /");
+    System.out.println("    5. Exit");
+    while(choice){
+      System.out.println("Enter your choice: ");
+      switchX=sc.nextInt();
+      switch(switchX){
+        case 1:
+          System.out.println("Enter 1st number: ");
+          one=sc.nextInt();
+          System.out.println("Enter 2nd number: ");
+          two=sc.nextInt();
+          result=one + two;
+          system.out.println("RESULT : "+ result);
+          break;
+  
+        case 2:
+          System.out.println("Enter 1st number: ");
+          one=sc.nextInt();
+          System.out.println("Enter 2nd number: ");
+          two=sc.nextInt();
+          result=one - two;
+          system.out.println("RESULT : "+ result);
+          break;
+        
+        case 3:
+          System.out.println("Enter 1st number: ");
+          one=sc.nextInt();
+          System.out.println("Enter 2nd number: ");
+          two=sc.nextInt();
+          result=one * two;
+          system.out.println("RESULT : "+ result);
+          break;
+  
+        case 4:
+          System.out.println("Enter 1st number: ");
+          one=sc.nextInt();
+          System.out.println("Enter 2nd number: ");
+          two=sc.nextInt();
+          result=one / two;
+          system.out.println("RESULT : "+ result);
+          break;
+        
+        case 5:
+          choice=false;
+          break;
+        
+        default:
+          system.out.println("Invalid Choice");
+          break;
+        
+      }
+    } 
+  }
+}
